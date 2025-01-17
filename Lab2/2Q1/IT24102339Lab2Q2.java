ac44208126e1f2ec0840ceb477fccbb18b4101b8
KAVINDU MADUSARA DISSANYAKA <164538300+kavindu599@users.noreply.github.com>
2025-01-17 13:50:33 +0530

    Add IT24102339

---
+++ Lab2/2Q1/IT24102339Lab2Q2.java
@@ -0,0 +1,24 @@
+public class IT24102339Lab2Q2 {
+ 
+      public static void main(String[]args) {
+
+            //Declared and initialyzed variables 
+            int perimeter = 100;
+    
+             double length, width, x;
+     
+             //Calculation
+             x= perimeter*(2.0/7.0);
+             length = x;
+             width = (3.0/4.0)*x;
+
+             //Print the output
+             System.out.println(length);
+             System.out.println(width);
+
+          }
+
+
+
+
+}
